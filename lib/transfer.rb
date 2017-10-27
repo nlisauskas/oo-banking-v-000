c4033659760c4841e85f2f26ea826af4f9120f61
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-10-27 03:09:49 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,7 +22,7 @@ class Transfer
     @receiver.balance += @amount
     @sender.balance -= @amount
     @status = "complete"
-  else end
+  else return "Transaction rejected. Please check your account balance." end
   end
 
 end

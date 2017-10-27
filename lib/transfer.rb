62b04a86ec3e243ea41a767165bfe5af5ee3efd8
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-10-27 03:16:05 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,15 +18,11 @@ class Transfer
   end
 
   def execute_transaction
-    i = 0
-    while i == 0
     if sender.valid? == true then
     @receiver.balance += @amount
     @sender.balance -= @amount
     @status = "complete"
   else return "Transaction rejected. Please check your account balance." end
-    i += 1
-  end
   end
 
 end

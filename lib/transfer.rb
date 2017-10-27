f7fcae94eacbb69aab0f7b6213501eb21091af23
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-10-27 03:19:58 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,6 +24,8 @@ class Transfer
     self.status = "complete"
   else
     self.status = "rejected"
-    return "Transaction rejected. Please check your account balance." 
+    return "Transaction rejected. Please check your account balance."
   end
+end
+  
 end

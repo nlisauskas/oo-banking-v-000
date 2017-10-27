baf7f07cd756b96c700eddc3095b84b104ea02d3
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-10-27 03:23:36 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -10,7 +10,7 @@ class Transfer
   end
 
   def valid?
-    sender.valid?(sender.balance - amount) && receiver.valid? true : false
+    sender.valid?(sender.balance - amount) && receiver.valid? ? true : false
   end
 
   def execute_transaction
